703bd1663a0cd3d2b18c6e32c925b94c5200f54b
Galileu Batista <gbatmobile@gmail.com>
2025-08-05 18:37:20 -0300

    Validacao de CPF

---
+++ euller-01-v1.py
@@ -0,0 +1,18 @@
+soma = 0
+x = 1
+
+if (x % 3 == 0) or (x % 5 == 0):
+    soma = soma + x
+x = x + 1
+
+if (x % 3 == 0) or (x % 5 == 0):
+    soma = soma + x
+x = x + 1
+
+if (x % 3 == 0) or (x % 5 == 0):
+    soma = soma + x
+x = x + 1
+
+# ...
+
+
